2ce2a80d2f969b528b1970ea32e66502daad80a4
Maya Rebholz <maya.rebholz@gmail.com>
2026-04-06 13:00:17 -0400

    milestone 1

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -4,4 +4,11 @@
 # Activate the virtual environment and start Jupyter
 cd "$(dirname "$0")"
 source ../accelforge_env/bin/activate
+
+echo "=== Starting Jupyter Notebook ==="
+echo ""
+echo "IMPORTANT: In your notebook, select the kernel:"
+echo "  Kernel -> Change Kernel -> Python (accelforge_env)"
+echo ""
+
 jupyter notebook
